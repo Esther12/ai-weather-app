7590fb43add7e2ee3a90cdfdce7170d681c5547e
Yating su <yatingsu@Spongebob-Pro.local>
2025-05-20 21:56:05 -0400

    set up

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Esther12.github.io/ai-weather-app',
   plugins: [react()],
   resolve: {
     alias: {
